3c9155f43ee8299d9ddd3c78288b3433030dc477
esouqu <nippl3juic3@gmail.com>
2022-08-04 17:40:45 +0700

    refactor: rework slider

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
-const buttons = document.querySelectorAll('.slider__button');
-const sliderImages = document.querySelectorAll('.slider__image');
-const sliderItems = document.querySelector('.slider-images-wrapper');
+const SLIDE_AREA = document.querySelector('.slider__slide-area');
+const SLIDER_BUTTONS = document.querySelectorAll('.slider__button');
+const SLIDER_DOTS = document.querySelectorAll('.slider__dot');
 
-buttons[1].classList.add('active');
+let dotCount = 1;
 
-sliderImages.forEach(el => el.addEventListener('click', changeImage));
-buttons.forEach(el => el.addEventListener('click', changeImage));
-function changeImage(event) {
-  buttons.forEach(el => el.classList.remove('active'));
+SLIDER_DOTS[1].classList.add('active');
 
-  if (event.target === buttons[0] || event.target === sliderImages[0]) {
-    sliderItems.style.transform = "translateX(860px)";
-    buttons[0].classList.add('active');
+SLIDER_BUTTONS.forEach(el => el.addEventListener('click', moveSlider));
+
+function moveSlider() {
+  const SLIDES = [...document.querySelectorAll('.slider-image-wrapper')];
+
+  SLIDER_DOTS.forEach(dot => dot.classList.remove('active'));
+  SLIDER_BUTTONS.forEach(el => el.removeEventListener('click', moveSlider));
+  
+  //slide to left
+  if (this.innerHTML === '⮜') {
+    SLIDES.push(SLIDES[SLIDES.length - 1]);
+    SLIDE_AREA.prepend(SLIDES[SLIDES.length - 1]);
+
+    SLIDE_AREA.style.animation = '.4s moveLeft forwards';
+
+    //keep track of active dot position
+    if (dotCount === 0) {
+      dotCount = 2;
+    } else {
+      dotCount --;
+    };
+    SLIDER_DOTS[dotCount].classList.add('active');
   };
-  if (event.target === buttons[1] || event.target === sliderImages[1]) {
-    sliderItems.style.transform = "translateX(0px)";
-    buttons[1].classList.add('active');
+
+  //slide to right
+  if (this.innerHTML === '⮞') {
+    SLIDES.push(SLIDES[0]);
+    SLIDE_AREA.appendChild(SLIDES[0]);
+
+    SLIDE_AREA.style.animation = '.4s moveRight forwards';
+
+    if (dotCount === 2) {
+      dotCount = 0;
+    } else {
+      dotCount ++;
+    };
+    SLIDER_DOTS[dotCount].classList.add('active');
   };
-  if (event.target === buttons[2] || event.target === sliderImages[2]) {
-    sliderItems.style.transform = "translateX(-860px)";
-    buttons[2].classList.add('active');
-  };
+  
+  setTimeout(() => {
+    SLIDE_AREA.style.animation = 'none';
+    SLIDER_BUTTONS.forEach(el => el.addEventListener('click', moveSlider));
+  }, 400)
 };
